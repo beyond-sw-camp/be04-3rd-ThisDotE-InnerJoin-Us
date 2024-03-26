adb9f463e26604f6b195e7285f2fcbb27293abc0
Bodrami <dlwldms1523@gmail.com>
2024-03-26 18:41:15 +0900

    임시  push

---
+++ src/views/ViewAllArticleHot/ViewAllHotStudyGroupArticle/ViewAllHotStudyGroupArticle.vue
@@ -0,0 +1,264 @@
+<template>
+	<head>
+		<meta charset="utf-8"/>
+		<link
+			rel="stylesheet"
+			href="../../../assets/css/ViewAllArticleHot/ViewAllHotStudyGroupArticle/globals.css"
+		/>
+		<link
+			rel="stylesheet"
+			href="../../../assets/css/ViewAllArticleHot/ViewAllHotStudyGroupArticle/style.css"
+		/>
+	</head>
+
+	<body>
+		<div class="div-wrapper">
+			<div class="div">
+				<div class="group">
+					<div class="overlap-group-wrapper">
+						<div class="overlap-group">
+							<div class="text-wrapper">스터디 그룹</div>
+						</div>
+					</div>
+					<div class="text-wrapper-2">질문</div>
+					<div class="text-wrapper-3">정보 공유</div>
+				</div>
+				<div class="group-2">
+					<img class="image" src="img/image.png" />
+					<div class="group-3">
+						<div class="overlap-group-2">
+							<div class="text-wrapper-4">작성하기</div>
+							<img class="ph-pencil-fill" src="img/ph-pencil-fill.png" />
+						</div>
+					</div>
+					<div class="overlap-wrapper">
+						<div class="overlap">
+							<div class="text-wrapper-5">인기순</div>
+							<img class="img" src="img/group.png" />
+						</div>
+					</div>
+				</div>
+
+
+				<div class="group-4">
+					<div class="overlap-2">
+						<div class="overlap-3">
+							<img class="innerjoin-us" src="img/innerjoin-us.png" />
+							<img class="image-2" src="img/1.png" />
+						</div>
+						<div class="view">
+							<div class="overlap-group-3">
+								<div class="text-wrapper-6">검색</div>
+								<img class="mdi-magnify" src="img/mdi-magnify.png" />
+							</div>
+						</div>
+						<div class="vector-wrapper">
+							<img class="vector" src="img/vector.png" />
+						</div>
+					</div>
+				</div>
+
+				<div class="group-5">
+					<div class="img-wrapper">
+						<img class="vector-2" src="img/vector-1.png" />
+					</div>
+					<img class="image-3" src="img/2.png" />
+					<div class="overlap-4">
+						<div class="text-wrapper-7">boraPower</div>
+						<div class="text-wrapper-8">2시간 전</div>
+					</div>
+					<div class="element">2&nbsp;&nbsp;/&nbsp;&nbsp;7</div>
+					<p class="springboot">
+						springboot로 기능 구현 하실 수 있는 분들을 모집합니다. 프로젝트
+						경험이 있거나 관심이 있으신 <br />분들은 거부감 없이 모두
+						모여주세요~
+					</p>
+					<div class="text-wrapper-9">2023.03.25</div>
+					<p class="p">
+						애니메이션 커뮤니티 함께 개발하실 백엔드 개발자분들 모집합니다!
+					</p>
+					<img class="group-6" src="img/group-679.png" />
+					<div class="text-wrapper-10">100</div>
+					<div class="text-wrapper-11">100</div>
+					<div class="group-7">
+						<div class="text-wrapper-12">모집정원</div>
+						<img class="vector-3" src="img/vector-2.png" />
+					</div>
+					<div class="group-8">
+						<div class="text-wrapper-12">모집기한</div>
+						<img class="vector-4" src="img/vector-3.png" />
+					</div>
+					<img class="vector-5" src="img/vector-4.png" />
+				</div>
+				<div class="group-9">
+					<div class="img-wrapper">
+						<img class="vector-2" src="img/vector-21.png" />
+					</div>
+					<img class="image-3" src="img/7.png" />
+					<div class="overlap-4">
+						<div class="text-wrapper-7">boraPower</div>
+						<div class="text-wrapper-8">2시간 전</div>
+					</div>
+					<div class="element">2&nbsp;&nbsp;/&nbsp;&nbsp;7</div>
+					<p class="springboot">
+						springboot로 기능 구현 하실 수 있는 분들을 모집합니다. 프로젝트
+						경험이 있거나 관심이 있으신 <br />분들은 거부감 없이 모두
+						모여주세요~
+					</p>
+					<div class="text-wrapper-9">2023.03.25</div>
+					<p class="p">
+						애니메이션 커뮤니티 함께 개발하실 백엔드 개발자분들 모집합니다!
+					</p>
+					<img class="group-6" src="img/group-679-5.png" />
+					<div class="text-wrapper-10">100</div>
+					<div class="text-wrapper-11">100</div>
+					<div class="group-7">
+						<div class="text-wrapper-12">모집정원</div>
+						<img class="vector-3" src="img/vector-22.png" />
+					</div>
+					<div class="group-8">
+						<div class="text-wrapper-12">모집기한</div>
+						<img class="vector-4" src="img/vector-23.png" />
+					</div>
+					<img class="vector-5" src="img/vector-24.png" />
+				</div>
+				<div class="group-10">
+					<div class="img-wrapper">
+						<img class="vector-2" src="img/vector-21.png" />
+					</div>
+					<img class="image-3" src="img/7.png" />
+					<div class="overlap-4">
+						<div class="text-wrapper-7">boraPower</div>
+						<div class="text-wrapper-8">2시간 전</div>
+					</div>
+					<div class="element">2&nbsp;&nbsp;/&nbsp;&nbsp;7</div>
+					<p class="springboot">
+						springboot로 기능 구현 하실 수 있는 분들을 모집합니다. 프로젝트
+						경험이 있거나 관심이 있으신 <br />분들은 거부감 없이 모두
+						모여주세요~
+					</p>
+					<div class="text-wrapper-9">2023.03.25</div>
+					<p class="p">
+						애니메이션 커뮤니티 함께 개발하실 백엔드 개발자분들 모집합니다!
+					</p>
+					<img class="group-6" src="img/group-679-5.png" />
+					<div class="text-wrapper-10">100</div>
+					<div class="text-wrapper-11">100</div>
+					<div class="group-7">
+						<div class="text-wrapper-12">모집정원</div>
+						<img class="vector-3" src="img/vector-22.png" />
+					</div>
+					<div class="group-8">
+						<div class="text-wrapper-12">모집기한</div>
+						<img class="vector-4" src="img/vector-23.png" />
+					</div>
+					<img class="vector-5" src="img/vector-24.png" />
+				</div>
+				<div class="group-11">
+					<div class="img-wrapper">
+						<img class="vector-2" src="img/vector-21.png" />
+					</div>
+					<img class="image-3" src="img/7.png" />
+					<div class="overlap-4">
+						<div class="text-wrapper-7">boraPower</div>
+						<div class="text-wrapper-8">2시간 전</div>
+					</div>
+					<div class="element">2&nbsp;&nbsp;/&nbsp;&nbsp;7</div>
+					<p class="springboot">
+						springboot로 기능 구현 하실 수 있는 분들을 모집합니다. 프로젝트
+						경험이 있거나 관심이 있으신 <br />분들은 거부감 없이 모두
+						모여주세요~
+					</p>
+					<div class="text-wrapper-9">2023.03.25</div>
+					<p class="p">
+						애니메이션 커뮤니티 함께 개발하실 백엔드 개발자분들 모집합니다!
+					</p>
+					<img class="group-6" src="img/group-679-5.png" />
+					<div class="text-wrapper-10">100</div>
+					<div class="text-wrapper-11">100</div>
+					<div class="group-7">
+						<div class="text-wrapper-12">모집정원</div>
+						<img class="vector-3" src="img/vector-22.png" />
+					</div>
+					<div class="group-8">
+						<div class="text-wrapper-12">모집기한</div>
+						<img class="vector-4" src="img/vector-23.png" />
+					</div>
+					<img class="vector-5" src="img/vector-24.png" />
+				</div>
+				<div class="group-12">
+					<div class="img-wrapper">
+						<img class="vector-2" src="img/vector-21.png" />
+					</div>
+					<img class="image-3" src="img/7.png" />
+					<div class="overlap-4">
+						<div class="text-wrapper-7">boraPower</div>
+						<div class="text-wrapper-8">2시간 전</div>
+					</div>
+					<div class="element">2&nbsp;&nbsp;/&nbsp;&nbsp;7</div>
+					<p class="springboot">
+						springboot로 기능 구현 하실 수 있는 분들을 모집합니다. 프로젝트
+						경험이 있거나 관심이 있으신 <br />분들은 거부감 없이 모두
+						모여주세요~
+					</p>
+					<div class="text-wrapper-9">2023.03.25</div>
+					<p class="p">
+						애니메이션 커뮤니티 함께 개발하실 백엔드 개발자분들 모집합니다!
+					</p>
+					<img class="group-6" src="img/group-679-5.png" />
+					<div class="text-wrapper-10">100</div>
+					<div class="text-wrapper-11">100</div>
+					<div class="group-7">
+						<div class="text-wrapper-12">모집정원</div>
+						<img class="vector-3" src="img/vector-22.png" />
+					</div>
+					<div class="group-8">
+						<div class="text-wrapper-12">모집기한</div>
+						<img class="vector-4" src="img/vector-23.png" />
+					</div>
+					<img class="vector-5" src="img/vector-24.png" />
+				</div>
+				<div class="group-13">
+					<div class="img-wrapper">
+						<img class="vector-2" src="img/vector-21.png" />
+					</div>
+					<img class="image-3" src="img/7.png" />
+					<div class="overlap-4">
+						<div class="text-wrapper-7">boraPower</div>
+						<div class="text-wrapper-8">2시간 전</div>
+					</div>
+					<div class="element">2&nbsp;&nbsp;/&nbsp;&nbsp;7</div>
+					<p class="springboot">
+						springboot로 기능 구현 하실 수 있는 분들을 모집합니다. 프로젝트
+						경험이 있거나 관심이 있으신 <br />분들은 거부감 없이 모두
+						모여주세요~
+					</p>
+					<div class="text-wrapper-9">2023.03.25</div>
+					<p class="p">
+						애니메이션 커뮤니티 함께 개발하실 백엔드 개발자분들 모집합니다!
+					</p>
+					<img class="group-6" src="img/group-679-5.png" />
+					<div class="text-wrapper-10">100</div>
+					<div class="text-wrapper-11">100</div>
+					<div class="group-7">
+						<div class="text-wrapper-12">모집정원</div>
+						<img class="vector-3" src="img/vector-22.png" />
+					</div>
+					<div class="group-8">
+						<div class="text-wrapper-12">모집기한</div>
+						<img class="vector-4" src="img/vector-23.png" />
+					</div>
+					<img class="vector-5" src="img/vector-24.png" />
+				</div>
+			</div>
+		</div>
+	</body>
+</template>
+
+<script setup>
+    
+</script>
+
+<style scoped>
+
+</style>
